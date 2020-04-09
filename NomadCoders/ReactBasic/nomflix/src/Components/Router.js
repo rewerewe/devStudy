8ebe58b83bd97673790c48f07ca352ed7954714d
meiis <rewerewe113@gmail.com>
2020-04-09 22:54:43 +0900

    create styled-components

--- NomadCoders/ReactBasic/nomflix/src/Components/Router.js
+++ NomadCoders/ReactBasic/nomflix/src/Components/Router.js
@@ -1,16 +1,20 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
-import HOME from "Routes/Home";
-import TV from "Routes/TV";
-import Search from "Routes/Search";
+import React from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import Header from 'Components/Header';
+import HOME from 'Routes/Home';
+import TV from 'Routes/TV';
+import Search from 'Routes/Search';
 
 export default () => (
-  <Router>
-    <Switch>
-      <Route path="/" exact component={HOME} />
-      <Route path="/tv" exact component={TV} />
-      <Route path="/search" component={Search} />
-      <Redirect from="*" to="/" />
-    </Switch>
-  </Router>
+	<Router>
+    <>
+    <Header />
+		<Switch>
+			<Route path="/" exact component={HOME} />
+			<Route path="/tv" exact component={TV} />
+			<Route path="/search" component={Search} />
+			<Redirect from="*" to="/" />
+		</Switch>
+    </>
+	</Router>
 );
