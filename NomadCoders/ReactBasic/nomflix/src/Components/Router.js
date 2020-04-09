f7908c1708a3521b6da73d6c8a5bda61cef096e6
meiis <rewerewe113@gmail.com>
2020-04-09 21:45:37 +0900

    Add Header.js and Redirect, Switch

--- NomadCoders/ReactBasic/nomflix/src/Components/Router.js
+++ NomadCoders/ReactBasic/nomflix/src/Components/Router.js
@@ -1,13 +1,16 @@
 import React from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 import HOME from "Routes/Home";
 import TV from "Routes/TV";
 import Search from "Routes/Search";
 
 export default () => (
   <Router>
-    <Route path="/" exact component={HOME} />
-    <Route path="/tv" exact component={TV} />
-    <Route path="/search" exact component={Search} />
+    <Switch>
+      <Route path="/" exact component={HOME} />
+      <Route path="/tv" exact component={TV} />
+      <Route path="/search" component={Search} />
+      <Redirect from="*" to="/" />
+    </Switch>
   </Router>
 );
