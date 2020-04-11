f91bf51041a7671f464ac2090b7356b3148f9f7b
meiis <rewerewe113@gmail.com>
2020-04-12 01:18:10 +0900

    Add API

--- NomadCoders/ReactBasic/nomflix/src/api.js
+++ NomadCoders/ReactBasic/nomflix/src/api.js
@@ -4,10 +4,44 @@ const api = axios.create({
   baseURL: "https://api.themoviedb.org/3/",
   params: {
     api_key: "4d19206ceb3ea471471a67257e87619a",
-    language: "en-US",
-  },
+    language: "en-US"
+  }
 });
 
 api.get("tv/popular");
 
-export default api;
+export const tvApi = {
+  nowPlaying: () => api.get("movie/now_playing"),
+  upcoming: () => api.get("movie/upcoming"),
+  popular: () => api.get("movie/popular"),
+  movieDetail: (id) =>
+    api.get(`movie/${id}`, {
+      params: {
+        append_to_response: "videos"
+      }
+    }),
+  search: (term) =>
+    api.get("search/movie", {
+      params: {
+        query: encodeURIComponent(term)
+      }
+    })
+};
+
+export const movieApi = {
+  topRated: () => api.get("tv/top_rated"),
+  popular: () => api.get("tv/popular"),
+  airingToday: () => api.get("tv/airing_today"),
+  showDetail: (id) =>
+    api.get(`tv/${id}`, {
+      params: {
+        append_to_response: "videos"
+      }
+    }),
+  search: (term) =>
+    api.get("search/tv", {
+      params: {
+        query: encodeURIComponent(term)
+      }
+    })
+};
