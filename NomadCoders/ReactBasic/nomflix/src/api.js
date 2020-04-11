13904a95cb64863f3d5700ab29086d85305c9a56
meiis <rewerewe113@gmail.com>
2020-04-12 02:42:50 +0900

    Update error

--- NomadCoders/ReactBasic/nomflix/src/api.js
+++ NomadCoders/ReactBasic/nomflix/src/api.js
@@ -10,7 +10,7 @@ const api = axios.create({
 
 api.get("tv/popular");
 
-export const tvApi = {
+export const moviesApi = {
   nowPlaying: () => api.get("movie/now_playing"),
   upcoming: () => api.get("movie/upcoming"),
   popular: () => api.get("movie/popular"),
@@ -28,7 +28,7 @@ export const tvApi = {
     })
 };
 
-export const movieApi = {
+export const tvApi = {
   topRated: () => api.get("tv/top_rated"),
   popular: () => api.get("tv/popular"),
   airingToday: () => api.get("tv/airing_today"),
